3545e844e3100f6ade5c12f521a39d8f28640e57
jhygt46 <infolifeoperacional@gmail.com>
2021-01-06 21:57:00 -0300

    aa

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
     $db_host = "localhost";
-    $db_user = "usinox_usinox";
-    $db_password = "usinox";
-    $db_database = "usinox_usinox";
+    $db_user = "root";
+    $db_password = "xG7wS1Lp4EsY8qmB2aZLur5vc6Wks8sY";
+    $db_database = "usinox";
     $cookie_secure = null;
     $cookie_http_only = null;
 
