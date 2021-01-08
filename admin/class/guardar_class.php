c814b2b90771fdfb5542555c8909ae9dae9125fe
jhygt46 <infolifeoperacional@gmail.com>
2021-01-08 18:00:12 -0300

    aa

--- admin/class/guardar_class.php
+++ admin/class/guardar_class.php
@@ -720,16 +720,9 @@ class Guardar{
                     $imagename = $this->get_force_name_upload($filepath, $filename, strtolower($extension));
                     $tmp = $_FILES['file_image'.$i.$i]['tmp_name'];
                     if(move_uploaded_file($tmp, $filepath.$imagename)){
-                            $data = getimagesize($filepath.$imagename);
-                            $info['mime'] = $data['mime'];
-                            if($data['mime'] == "application/pdf"){
-                                $info['op'] = 1;
-                                $info['mensaje'] = "Imagen subida";
-                                $info['image'] = $imagename;
-                            }else{
-                                $info['op'] = 2;
-                                $info['mensaje'] = "La imagen no es jpg / jpeg";
-                            }
+                        $info['op'] = 1;
+                        $info['mensaje'] = "Imagen subida";
+                        $info['image'] = $imagename;
                     }else{
                         $info['op'] = 2;
                         $info['mensaje'] = "No se pudo subir la imagen";
