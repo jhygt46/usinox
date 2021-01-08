74342d0e6df59cb5f140548902c95cbc7409842c
jhygt46 <infolifeoperacional@gmail.com>
2021-01-08 00:12:04 -0300

    aa

--- admin/class/guardar_class.php
+++ admin/class/guardar_class.php
@@ -552,6 +552,7 @@ class Guardar{
         $info['FILE'] = $_FILES;
         
         $image = $this->upload($_SERVER["DOCUMENT_ROOT"]."/uploads", null, 0);
+        $info['image'] = $image;
         if($image['op'] == 1){
             if($sql = $this->con->prepare("INSERT INTO _usinox_productos_fotos (nombre, id_pro) VALUES (?, ?)")){
                 if($sql->bind_param("si", $image["image"], $id)){
