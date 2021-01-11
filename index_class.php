4e33d1e8d1f3aa2c6abadf4c3b3bfdb4e8db7269
jhygt46 <infolifeoperacional@gmail.com>
2021-01-11 02:56:55 -0300

    aa

--- index_class.php
+++ index_class.php
@@ -101,7 +101,7 @@ class Core{
         }else{ $this->htmlspecialchars($this->con->error); }
     }
     private function child_prods($id_cat){
-        if($sqls = $this->con->prepare("SELECT t1.id_pro, t1.nombre, t1.descripcion, t2.nombre as foto_nombre FROM _usinox_productos t1, _usinox_productos_fotos t2 WHERE t1.id_cat=? AND t1.id_pro=t2.id_pro AND t1.eliminado=?")){
+        if($sqls = $this->con->prepare("SELECT t1.id_pro, t1.nombre, t1.descripcion, t2.nombre as foto_nombre FROM _usinox_productos t1 LEFT JOIN _usinox_productos_fotos t2 ON t1.id_pro=t2.id_pro WHERE t1.id_cat=?  AND t1.eliminado=?")){
             if($sqls->bind_param("ii", $id_cat, $this->eliminado)){
                 if($sqls->execute()){
 
@@ -112,7 +112,11 @@ class Core{
                         $res[$id]["id_pro"] = $datas[$i]["id_pro"];
                         $res[$id]["nombre"] = $datas[$i]["nombre"];
                         $res[$id]["descripcion"] = $datas[$i]["descripcion"];
-                        $res[$id]["fotos"][] = $datas[$i]["foto_nombre"];
+                        if($datas[$i]["foto_nombre"] != null){
+                            $res[$id]["fotos"][] = $datas[$i]["foto_nombre"];
+                        }else{
+                            $res[$id]["fotos"][0] = "sin_imagen.jpg";
+                        }
                     }
                     return $datas;
 
