252b14d3ac47f6f3f30b0ec6ba3b15f462bbd93b
jhygt46 <infolifeoperacional@gmail.com>
2021-01-11 17:41:56 -0300

    aa

--- index.php
+++ index.php
@@ -89,20 +89,9 @@
                                         */
                                     ?>
                                     <?php if(isset($inicio['pro'])){ ?>
-                                    <div class="producto">
-                                        <div class="pro">
-                                            <div class="c_pro">
-                                                <div class="c_pfoto">
-                                                    <a class="link" href="<?php echo $valor['urls']; ?>"><img src="https://www.usinox.cl/foto.php?archivo=http://35.202.149.15/uploads/images/<?php echo $valor['fotos'][0]; ?>&ancho=218&alto=180" alt="" /></a>
-                                                </div>
-                                                <div class="c_pttl"><?php echo $valor['nombre']; ?></div>
-                                                <div class="c_pdesc"><?php echo $valor['descripcion']; ?></div>
-                                                <div class="c_pbtn clearfix">
-                                                    <div class="btn_cotizar">Cotizar</div>
-                                                    <div class="btn_mostrar btn_mostrar_prod"><div class="txt">Mostrar</div><div class="punto valign"></div></div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                    <div class="detalle_producto">
+                                        <div class="pro_info"></div>
+                                        <div class="pro_fotos"></div>
                                     </div>
                                     <?php } ?>
                                     <?php if(isset($inicio['childs_pro'])){ foreach ($inicio['childs_pro'] as $valor){ ?>
