cc5e5679bbe7c1c34010e4c4060868364ee9c37d
jhygt46 <infolifeoperacional@gmail.com>
2021-01-13 18:57:01 -0300

    aa

--- includes/_pagina_cotizador.php
+++ includes/_pagina_cotizador.php
@@ -5,17 +5,16 @@
             <div class="cot_pro_foto"><img class="valign" src="https://www.usinox.cl/foto.php?archivo=<?php echo $_SERVER["REQUEST_SCHEME"]; ?>://<?php echo $_SERVER["HTTP_HOST"]; ?>/uploads/images/<?php echo $cot_pro["foto_nombre"]; ?>&ancho=218&alto=180" alt="" /></div>
             <div class="cot_pro_info">
                 <table cellspacing="0" cellpadding="0" border="0" width="100%" height="100%" bgcolor="#f00">
-                    <tr><td></td></tr>
+                    <tr>
+                        <td valign="middle">
+                            <div class="cot_pro_titulo"><?php echo $cot_pro["nombre"]; ?></div>
+                            <div class="cot_pro_desc"><?php echo $cot_pro["descripcion"]; ?></div>
+                        </td>
+                    </tr>
                 </table>
-                <!--
-                <div class="valign">
-                    <div class="cot_pro_titulo"><?php echo $cot_pro["nombre"]; ?></div>
-                    <div class="cot_pro_desc"><?php echo $cot_pro["descripcion"]; ?></div>
-                </div>
-                -->
             </div>
-            <div class="cot_pro_cant"><input type="text" value="<?php echo $cot_cant; ?>" /></div>
-            <div class="cot_pro_del"><img src="" alt=""></div>
+            <div class="cot_pro_cant"><input class="valign" type="text" value="<?php echo $cot_cant; ?>" /></div>
+            <div class="cot_pro_del"><img class="valign" src="" alt=""></div>
         </div>
     <?php } ?>
 </div>
