cf40a0f30f734226392a9f73ad9f56d77cb4cadf
jhygt46 <infolifeoperacional@gmail.com>
2021-01-13 17:24:37 -0300

    aa

--- includes/_pagina_cotizador.php
+++ includes/_pagina_cotizador.php
@@ -2,7 +2,7 @@
 <div class="lista_cotizador">
     <?php for($i=0; $i<count($_SESSION["prods"]); $i++){ $cot_pro = $core->get_prod($_SESSION["prods"][$i]['id']); $cot_cant = $_SESSION["prods"][$i]['cant']; ?>
         <div class="cot_pro">
-            <div class="cot_pro_foto"><img src="" alt=""></div>
+            <div class="cot_pro_foto"><img src="https://www.usinox.cl/foto.php?archivo=<?php echo $_SERVER["REQUEST_SCHEME"]; ?>://<?php echo $_SERVER["HTTP_HOST"]; ?>/uploads/images/<?php echo $cot_pro["foto_nombre"][0]; ?>&ancho=218&alto=180" alt="" /></div>
             <div class="cot_pro_info">
                 <div class="cot_pro_titulo"><?php echo $cot_pro["nombre"]; ?></div>
                 <div class="cot_pro_desc"><?php echo $cot_pro["descripcion"]; ?></div>
