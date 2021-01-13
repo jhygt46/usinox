dabdeee7285fb8a4d88dae6a0a5c1589ee3d21ec
jhygt46 <infolifeoperacional@gmail.com>
2021-01-13 18:36:18 -0300

    aa

--- includes/_pagina_cotizador.php
+++ includes/_pagina_cotizador.php
@@ -4,10 +4,15 @@
         <div class="cot_pro clearfix">
             <div class="cot_pro_foto"><img src="https://www.usinox.cl/foto.php?archivo=<?php echo $_SERVER["REQUEST_SCHEME"]; ?>://<?php echo $_SERVER["HTTP_HOST"]; ?>/uploads/images/<?php echo $cot_pro["foto_nombre"]; ?>&ancho=218&alto=180" alt="" /></div>
             <div class="cot_pro_info">
+                <table cellspacing="0" cellpadding="0" border="0" width="100%" height="100%" bgcolor="#f00">
+                    <tr><td></td></tr>
+                </table>
+                <!--
                 <div class="valign">
                     <div class="cot_pro_titulo"><?php echo $cot_pro["nombre"]; ?></div>
                     <div class="cot_pro_desc"><?php echo $cot_pro["descripcion"]; ?></div>
                 </div>
+                -->
             </div>
             <div class="cot_pro_cant"><input type="text" value="<?php echo $cot_cant; ?>" /></div>
             <div class="cot_pro_del"><img src="" alt=""></div>
