4f80e96a4ad6caa43f2f89c1ab8cbb95eb58e119
jhygt46 <infolifeoperacional@gmail.com>
2021-01-13 15:21:36 -0300

    aa

--- ajax/index.php
+++ ajax/index.php
@@ -1,36 +1,40 @@
 <?php
+    session_start();
 
-if($_POST["accion"] == "cotizar"){
 
-    $count = 0;
-    $esta = false;
+    if($_POST["accion"] == "cotizar"){
 
-    for($i=0; $i<count($_SESSION["prods"]); $i++){
-        if($_SESSION["prods"][$i]['id'] == $_POST['id']){
-            if($_POST['cant'] == 1){
-                $_SESSION["prods"][$i]['cant'] = $_SESSION["prods"][$i]['cant'] + 1;
+        $count = 0;
+        $esta = false;
+
+        for($i=0; $i<count($_SESSION["prods"]); $i++){
+            if($_SESSION["prods"][$i]['id'] == $_POST['id']){
+                if($_POST['cant'] == 1){
+                    $_SESSION["prods"][$i]['cant'] = $_SESSION["prods"][$i]['cant'] + 1;
+                }
+                if($_POST['cant'] == 0){
+                    $_SESSION["prods"][$i]['cant'] = 0;
+                }
+                if($_POST['cant'] > 1){
+                    $_SESSION["prods"][$i]['cant'] = $_POST['cant'];
+                }
+                $esta = true;
             }
-            if($_POST['cant'] == 0){
-                $_SESSION["prods"][$i]['cant'] = 0;
-            }
-            if($_POST['cant'] > 1){
-                $_SESSION["prods"][$i]['cant'] = $_POST['cant'];
-            }
-            $esta = true;
+            $count = $count + $_SESSION["prods"][$i]['cant'];
         }
-        $count = $count + $_SESSION["prods"][$i]['cant'];
+
+        if(!$esta){
+
+            $count = $count + $_POST['cant'];
+            $ids = count($_SESSION["prods"]);
+            $_SESSION["prods"][$ids]['id'] = $_POST['id'];
+            $_SESSION["prods"][$ids]['cant'] = $_POST['cant'];
+
+        }
+
+        $data['count'] = $count;
+        echo json_encode($data);
+
     }
 
-    if(!$esta){
-
-        $count = $count + $_POST['cant'];
-        $ids = count($_SESSION["prods"]);
-        $_SESSION["prods"][$ids]['id'] = $_POST['id'];
-        $_SESSION["prods"][$ids]['cant'] = $_POST['cant'];
-
-    }
-
-    $data['count'] = $count;
-    echo json_encode($data);
-
-}
+?>
--- index.php
+++ index.php
@@ -1,87 +1,87 @@
-    <?php
-        session_start();
+<?php
+    session_start();
 
-        date_default_timezone_set('america/santiago');
-        require "index_class.php";
-        $core = new Core();
-        $inicio = $core->iniciar();
-        
-        
-        echo "<pre>";
-        print_r($_SESSION);
-        echo "</pre>";
+    date_default_timezone_set('america/santiago');
+    require "index_class.php";
+    $core = new Core();
+    $inicio = $core->iniciar();
+    
+    
+    echo "<pre>";
+    print_r($_SESSION);
+    echo "</pre>";
 
-        echo "<pre>";
-        print_r($inicio);
-        echo "</pre>";
-        
+    echo "<pre>";
+    print_r($inicio);
+    echo "</pre>";
+    
 
-    ?>
+?>
 
-    <html>
-        <head>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-            <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Google+Sans:400,500,700|Signika:300,400,500,700">
-            <script src="js/base.js" type="text/javascript"></script>
-            <link rel="stylesheet" type="text/css" href="css/base.css"/>
-        </head>
-        <body>
-            <div class="contenedor">
-                <div class="menu">
-                    <div class="btn_toogle" onclick="tooglemenu(this)"><div class="b b1"></div><div class="b b2"></div><div class="b b3"></div></div>
-                    <div class="titulo"></div>
-                    <div class="opciones"></div>
-                </div>
-                <div class="sitio">
-                    <?php include("includes/nav.php"); ?>
-                    <div class="medio clearfix">
-                        <div class="pmenu">
-                            <div class="nav">
-                                <div class="lp">CATEGORIAS</div>
-                                <?php for($i=0; $i<count($inicio['base']); $i++){ ?>
-                                <div class="ls"><a href="<?php echo $inicio['base'][$i]['urls']; ?>"><?php echo $inicio['base'][$i]['nombre']; ?></a></div>
-                                <?php } ?>
-                                <div class="ls"><a href="/todos-los-productos">Todos los Productos</a></div>
-                            </div>
-                            <div class="btnvideos">
-                                <a href="http://www.usinox.cl/videos" class="myButton">Ver Videos</a>
-                            </div>
-                            <a href="https://www.webpay.cl/portalpagodirecto/pages/institucion.jsf?idEstablecimiento=22102112" style="display: block; text-align: center"><img src="images/Webpay.jpg" alt=""></a>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Google+Sans:400,500,700|Signika:300,400,500,700">
+        <script src="js/base.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="css/base.css"/>
+    </head>
+    <body>
+        <div class="contenedor">
+            <div class="menu">
+                <div class="btn_toogle" onclick="tooglemenu(this)"><div class="b b1"></div><div class="b b2"></div><div class="b b3"></div></div>
+                <div class="titulo"></div>
+                <div class="opciones"></div>
+            </div>
+            <div class="sitio">
+                <?php include("includes/nav.php"); ?>
+                <div class="medio clearfix">
+                    <div class="pmenu">
+                        <div class="nav">
+                            <div class="lp">CATEGORIAS</div>
+                            <?php for($i=0; $i<count($inicio['base']); $i++){ ?>
+                            <div class="ls"><a href="<?php echo $inicio['base'][$i]['urls']; ?>"><?php echo $inicio['base'][$i]['nombre']; ?></a></div>
+                            <?php } ?>
+                            <div class="ls"><a href="/todos-los-productos">Todos los Productos</a></div>
                         </div>
-                        <div class="info">
-                            <div class="data">
-                                <?php include("includes/searchcart.php"); ?>
+                        <div class="btnvideos">
+                            <a href="http://www.usinox.cl/videos" class="myButton">Ver Videos</a>
+                        </div>
+                        <a href="https://www.webpay.cl/portalpagodirecto/pages/institucion.jsf?idEstablecimiento=22102112" style="display: block; text-align: center"><img src="images/Webpay.jpg" alt=""></a>
+                    </div>
+                    <div class="info">
+                        <div class="data">
+                            <?php include("includes/searchcart.php"); ?>
 
-                                <?php
+                            <?php
 
-                                    switch ($inicio["tipo"]) {
-                                        case "inicio":
-                                            include("includes/_pagina_inicio.php");
-                                            break;
-                                        case "categorias":
-                                            include("includes/_pagina_categorias.php");
-                                            break;
-                                        case "productos":
-                                            include("includes/_pagina_productos.php");
-                                            break;
-                                        case "producto":
-                                            include("includes/_pagina_detalle_producto.php");
-                                            break;
-                                        case "cotizador":
-                                            include("includes/_pagina_cotizador.php");
-                                            break;
-                                        case "paginas":
-                                            include("includes/_pagina_paginas.php");
-                                            break;
-                                    }
+                                switch ($inicio["tipo"]) {
+                                    case "inicio":
+                                        include("includes/_pagina_inicio.php");
+                                        break;
+                                    case "categorias":
+                                        include("includes/_pagina_categorias.php");
+                                        break;
+                                    case "productos":
+                                        include("includes/_pagina_productos.php");
+                                        break;
+                                    case "producto":
+                                        include("includes/_pagina_detalle_producto.php");
+                                        break;
+                                    case "cotizador":
+                                        include("includes/_pagina_cotizador.php");
+                                        break;
+                                    case "paginas":
+                                        include("includes/_pagina_paginas.php");
+                                        break;
+                                }
 
-                                ?>
+                            ?>
 
-                            </div>
                         </div>
                     </div>
-                    <div class="footer"></div>
                 </div>
+                <div class="footer"></div>
             </div>
-        </body>
-    </html>
+        </div>
+    </body>
+</html>
