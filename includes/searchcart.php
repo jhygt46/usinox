f0a3561eb49dcdc83c2d2aca2ae70b7bf13fe273
jhygt46 <infolifeoperacional@gmail.com>
2021-01-13 19:59:12 -0300

    aa

--- includes/searchcart.php
+++ includes/searchcart.php
@@ -9,7 +9,7 @@
         <div class="sh clearfix">
             <div class="l"></div>
             <div class="i"><input type="text" id="search" value=""></div>
-            <div class="s" onclick="search()"><a class="vhalign" href="">BUSCAR</a></div>
+            <div class="s" onclick="search()"><div class="vhalign" href="">BUSCAR</div></div>
         </div>
     </div>
     <a href="/cotizador" class="carro">
