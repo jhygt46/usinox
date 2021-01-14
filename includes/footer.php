04ae0dbac6a5025a12c3ba577b8288bdddc2f0d8
jhygt46 <infolifeoperacional@gmail.com>
2021-01-14 15:51:14 -0300

    aa

--- includes/footer.php
+++ includes/footer.php
@@ -2,20 +2,21 @@
     <div class="contfooter">
         <div class="foo clearfix">
             <div class="map">
+            <div class="titulo">Mapa de Ubicaci&oacute;n</div>
                 <div id="googleMap"></div>
             </div>
             <div class="foo_info">
-                <div class="titulo">Informacion de la Empresa</div>
-                <div class="tipo1">Telefono:</div>
+                <div class="titulo">Informaci&oacute;n de la Empresa</div>
+                <div class="tipo1">Teléfono:</div>
                 <div class="tipo2"><a href="tel:56 2 2594 9797">+56 2 2594 9797</a></div>
-                <div class="tipo1">Correo electronico:</div>
+                <div class="tipo1">Correo electr&oacute;nico:</div>
                 <div class="tipo2"><a href="mailto:info@usinox.cl">ventas@usinox.cl</a></div>
-                <div class="tipo1">Direccion:</div>
+                <div class="tipo1">Direcci&oacute;n:</div>
                 <div class="tipo2">Las Encinas 721, Cerrillos, Santiago de Chile</div>
                 <div class="tipo1">Webpay:</div>
                 <div class="tipo2"><a href="https://www.webpay.cl/portalpagodirecto/pages/institucion.jsf?idEstablecimiento=22102112">Pagar con Webpay</a></div>
                 <div class="tipo1">Horario de Atenci&oacute;n:</div>
-                <div class="tipo2">Continuado de  Lunes a Viernes de 8:00 a 17:30 Hrs.</div>
+                <div class="tipo2">Continuado de Lunes a Viernes de 8:00 a 17:30 Hrs.</div>
             </div>
         </div>
     </div>
