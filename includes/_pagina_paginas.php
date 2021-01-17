b731e8993ab04189a9d0d1039231e1071b8a7435
jhygt46 <infolifeoperacional@gmail.com>
2021-01-17 19:01:16 -0300

    aa

--- includes/_pagina_paginas.php
+++ includes/_pagina_paginas.php
@@ -3,9 +3,8 @@
     <div class="nombre">Nosotros</div>
 </div>
 <div class="paginas">
-    <div class="cont_pagina clearfix">
-        <div class="pag_cont"><img src="/images/nosotros.jpg">
-        USINOX, Empresa con mas de 140 años de permanencia en el mercado, dedicada al Equipamiento de Cocinas Profesionales para Restaurantes, Hoteles, Instituciones, Supermercados, Campamentos Mineros, Fast-Food y la Gastronomia en General.<br/><br/>
+    <div class="cont_pagina">
+        <div class="pag_cont"><img src="/images/nosotros.jpg">USINOX, Empresa con mas de 140 años de permanencia en el mercado, dedicada al Equipamiento de Cocinas Profesionales para Restaurantes, Hoteles, Instituciones, Supermercados, Campamentos Mineros, Fast-Food y la Gastronomia en General.<br/><br/>
         De esta forma USINOX esta capacitado para proporcionar a sus clientes el mas completo servicio de asesoria en el estudio, diseño, proyectos, planos, fabricacion y puesta en marcha de un Equipamiento Integral que satisfaga las mas exigentes necesidades del Cliente.<br/><br/>
         El sistema de Usinox, ha sido adoptado y recibido con beneplacito por importantes Clientes en nuestro Pais y en el Extranjero, ya que hemos sido favorecidos con pedidos de Paises como; Argentina, Australia, Peru, Bolivia y Estados Unidos.
         </div>
@@ -26,13 +25,42 @@
 <?php if($inicio["pagina"] == "servicios"){ ?>
 <div class="titulo"><div class="nombre">Servicios</div></div>
 <div class="paginas">
-    <div class="cont_pagina clearfix">
-        <div class="pag_foto"><img src="/images/servicios_01.jpg"></div>
-        <div class="pag_info"></div>
+    <div class="cont_pagina">
+        <div class="pag_titulo">Servicio Técnico</div>
+        <div class="pag_cont"><img src="/images/servicios_01.jpg">El Servicio técnico de Usinox tiene una vasta experiencia en equipos gastronómicos de uso industrial. 140 años de historia en el mercado nos avalan, prestando una atención de calidad y garantía de satisfacción.<br/><br/>
+            Entregamos asesoramiento, capacitación en mantención y limpieza de equipos gastronómicos, con una atención personalizada.<br/><br/>
+            Consulte por los contratos de Mantención Preventiva anual que tenemos para cada tipo de negocio y según su necesidad.<br/>
+            Nuestros Clientes al adquirir un producto USINOX, tienen desde ya asegurado un servicio integral de post venta Profesional.<br/>
+            Ante cualquier solicitud, contáctenos al teléfono: +562 5549797 o bien a nuestro mail: serviciotecnico@usinox.cl
+        </div>
     </div>
-    <div class="cont_pagina clearfix">
-        <div class="pag_foto"><img src="/images/servicios_02.jpg"></div>
-        <div class="pag_info"></div>
+    <div class="cont_pagina">
+        <div class="pag_titulo">Nuestros Servicios</div>
+        <div class="pag_cont"><img src="/images/servicios_02.jpg">
+            <strong>SERVICIO DE CORTE</strong>
+            Guillotina Hidraulica de 3 Metros, Con capacidad de corte desde 0,6 mm, hasta 12mm, En acero 1010 acero inoxidable desde 0,6 hasta 6mm.
+
+            <strong>SERVICIO DE CILINDRADO</strong>
+            Cilindradora Mecanica de 2000 x 8 mm.
+            Cilindrado en fierro de 2000 x 8 mm.
+            Cilindrado de acero inoxidable de 2000 x 6 mm.
+
+            <strong>SERVICIO DE PUNZONADO</strong>
+            Servicio de punzonado con maquina multitool, con control cnc, de gran eficiencia, rapidez y flexibilidad, para los distintos tipos de trabajos.
+            Espesores de 0,6 mm., hasta 4 mm., en acero 1010.
+            Espesores de 0.6 mm., hasta 2 mm., en acero inoxidable.
+
+            <strong>SERVICIO DE ESTAMPADO</strong>
+            2 prensas hidraulicas de 360 toneladas y 150 toneladas, de doble efecto.
+
+            <strong>SERVICIO DE PLEGADO</strong>
+            Plegadora hidraulica con herramienta multiboca, control numerico de gran precision.
+
+            <strong>SERVICIO DE CURVADO</strong>
+            Curvadora de tubos redondos y cuadrados.
+            0 1" minimo hasta 50mm., en tubos.
+            Tubos cuadrados de 20 x 20, 25 x 25, 30 x 30, 40 x 40 y 50 x 50 mm.
+        </div>
     </div>
 </div>
 <?php } ?>
