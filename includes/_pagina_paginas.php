b722f2b162a16f352475c19ff8c9b048de3e5e63
jhygt46 <infolifeoperacional@gmail.com>
2021-01-17 18:54:13 -0300

    aa

--- includes/_pagina_paginas.php
+++ includes/_pagina_paginas.php
@@ -4,7 +4,7 @@
 </div>
 <div class="paginas">
     <div class="cont_pagina clearfix">
-        <div class="pag_foto"><img src="/images/nosotros.jpg">
+        <div class="pag_cont"><img src="/images/nosotros.jpg">
         USINOX, Empresa con mas de 140 años de permanencia en el mercado, dedicada al Equipamiento de Cocinas Profesionales para Restaurantes, Hoteles, Instituciones, Supermercados, Campamentos Mineros, Fast-Food y la Gastronomia en General.<br/><br/>
         De esta forma USINOX esta capacitado para proporcionar a sus clientes el mas completo servicio de asesoria en el estudio, diseño, proyectos, planos, fabricacion y puesta en marcha de un Equipamiento Integral que satisfaga las mas exigentes necesidades del Cliente.<br/><br/>
         El sistema de Usinox, ha sido adoptado y recibido con beneplacito por importantes Clientes en nuestro Pais y en el Extranjero, ya que hemos sido favorecidos con pedidos de Paises como; Argentina, Australia, Peru, Bolivia y Estados Unidos.
