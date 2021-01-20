3c64f44596c1646823d631b20a973f97befda680
jhygt46 <infolifeoperacional@gmail.com>
2021-01-20 04:25:59 -0300

    aa

--- includes/_pagina_productos.php
+++ includes/_pagina_productos.php
@@ -7,11 +7,11 @@
                 <div class="c_pfoto">
                     <a class="link" href="<?php echo $valor['urls']; ?>"><img src="https://www.usinox.cl/foto.php?archivo=<?php echo $_SERVER["REQUEST_SCHEME"]; ?>://<?php echo $_SERVER["HTTP_HOST"]; ?>/uploads/images/<?php echo $valor['fotos'][0]; ?>&ancho=218&alto=180" alt="" /></a>
                 </div>
-                <div class="c_pttl"><?php echo $valor['nombre']; ?></div>
-                <div class="c_pdesc"><?php echo substr($valor['descripcion'], 0, $limit_desc); ?></div>
+                <div class="c_pttl"><a class="link" href="<?php echo $valor['urls']; ?>"><?php echo $valor['nombre']; ?></a></div>
+                <div class="c_pdesc"><a class="link" href="<?php echo $valor['urls']; ?>"><?php echo substr($valor['descripcion'], 0, $limit_desc); ?></a></div>
                 <div class="c_pbtn clearfix">
                     <div class="btn_cotizar" onclick="cotizar(<?php echo $valor['id_pro']; ?>, 1)">Cotizar</div>
-                    <div class="btn_mostrar btn_mostrar_prod"><div class="txt">Mostrar</div><div class="punto valign"></div></div>
+                    <div class="btn_mostrar btn_mostrar_prod"><a class="link" href="<?php echo $valor['urls']; ?>"><div class="txt">Mostrar</div><div class="punto valign"></div></a></div>
                 </div>
             </div>
         </div>
