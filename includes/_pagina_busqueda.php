b33537cc18827b1a2763785d5dfb794187c83150
jhygt46 <infolifeoperacional@gmail.com>
2021-01-20 16:09:30 -0300

    aa

--- includes/_pagina_busqueda.php
+++ includes/_pagina_busqueda.php
@@ -1,20 +1,2 @@
 <div class="titulo"><div class="nombre">Busqueda</div></div>
-<div class="lista_productos clearfix">
-<?php if(isset($inicio['childs_pro'])){ foreach ($inicio['childs_pro'] as $valor){ ?>
-    <div class="producto">
-        <div class="pro">
-            <div class="c_pro">
-                <div class="c_pfoto">
-                    <a class="link" href="<?php echo $valor['urls']; ?>"><img src="https://www.usinox.cl/foto.php?archivo=<?php echo $_SERVER["REQUEST_SCHEME"]; ?>://<?php echo $_SERVER["HTTP_HOST"]; ?>/uploads/images/<?php echo $valor['fotos'][0]; ?>&ancho=218&alto=180" alt="" /></a>
-                </div>
-                <div class="c_pttl"><?php echo $valor['nombre']; ?></div>
-                <div class="c_pdesc"><?php echo substr($valor['descripcion'], 0, $limit_desc); ?></div>
-                <div class="c_pbtn clearfix">
-                    <div class="btn_cotizar" onclick="cotizar(<?php echo $valor['id_pro']; ?>, 1)">Cotizar</div>
-                    <div class="btn_mostrar btn_mostrar_prod"><div class="txt">Mostrar</div><div class="punto valign"></div></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php }} ?>
-</div>
+<?php include("includes/_pagina_lista_productos.php"); ?>
--- includes/_pagina_lista_productos.php
+++ includes/_pagina_lista_productos.php
@@ -1,3 +1,4 @@
 <div class="titulo"><div class="nombre">Vitrina de productos</div></div>
 <?php include("includes/_pagina_lista_productos.php"); ?>
 
+
