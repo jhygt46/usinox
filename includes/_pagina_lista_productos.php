c1354096f2e4d895d556b5407723df3602de8d3f
jhygt46 <infolifeoperacional@gmail.com>
2021-01-20 19:45:39 -0300

    aa

--- includes/_pagina_lista_productos.php
+++ includes/_pagina_lista_productos.php
@@ -7,7 +7,7 @@
                         <a class="link" href="<?php echo $valor['urls']; ?>"><img src="https://www.usinox.cl/foto.php?archivo=<?php echo $_SERVER["REQUEST_SCHEME"]; ?>://<?php echo $_SERVER["HTTP_HOST"]; ?>/uploads/images/<?php echo $valor['fotos'][0]; ?>&ancho=218&alto=180" alt="" /></a>
                     </div>
                     <div class="c_pttl"><a class="link" href="<?php echo $valor['urls']; ?>"><?php echo $valor['nombre']; ?></a></div>
-                    <div class="c_pdesc"><a class="link" href="<?php echo $valor['urls']; ?>"><?php echo substr($valor['descripcion'], 0, $limit_desc); ?></a></div>
+                    <div class="c_pdesc"><a class="link" href="<?php echo $valor['urls']; ?>"><?php echo $valor['descripcion']; ?></a></div>
                     <div class="c_pbtn clearfix">
                         <div class="btn_cotizar" onclick="cotizar(<?php echo $valor['id_pro']; ?>, 1)">Cotizar</div>
                         <div class="btn_mostrar btn_mostrar_prod"><a class="link" href="<?php echo $valor['urls']; ?>"><div class="txt">Mostrar</div><div class="punto valign"></div></a></div>
