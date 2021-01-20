2d3154eda2864130e150e8a14fa5cbd98d2686c3
jhygt46 <infolifeoperacional@gmail.com>
2021-01-20 16:10:33 -0300

    aa

--- includes/_pagina_lista_productos.php
+++ includes/_pagina_lista_productos.php
@@ -1,4 +1,21 @@
 <div class="titulo"><div class="nombre">Vitrina de productos</div></div>
-<?php include("includes/_pagina_lista_productos.php"); ?>
-
+<div class="lista_productos clearfix">
+    <?php if(isset($inicio['childs_pro'])){ foreach ($inicio['childs_pro'] as $valor){ ?>
+        <div class="producto">
+            <div class="pro">
+                <div class="c_pro">
+                    <div class="c_pfoto">
+                        <a class="link" href="<?php echo $valor['urls']; ?>"><img src="https://www.usinox.cl/foto.php?archivo=<?php echo $_SERVER["REQUEST_SCHEME"]; ?>://<?php echo $_SERVER["HTTP_HOST"]; ?>/uploads/images/<?php echo $valor['fotos'][0]; ?>&ancho=218&alto=180" alt="" /></a>
+                    </div>
+                    <div class="c_pttl"><a class="link" href="<?php echo $valor['urls']; ?>"><?php echo $valor['nombre']; ?></a></div>
+                    <div class="c_pdesc"><a class="link" href="<?php echo $valor['urls']; ?>"><?php echo substr($valor['descripcion'], 0, $limit_desc); ?></a></div>
+                    <div class="c_pbtn clearfix">
+                        <div class="btn_cotizar" onclick="cotizar(<?php echo $valor['id_pro']; ?>, 1)">Cotizar</div>
+                        <div class="btn_mostrar btn_mostrar_prod"><a class="link" href="<?php echo $valor['urls']; ?>"><div class="txt">Mostrar</div><div class="punto valign"></div></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php }} ?>
+</div>
 
