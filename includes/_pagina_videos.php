ed962e0503495f4e88d024c2b2a44310a19369f5
jhygt46 <infolifeoperacional@gmail.com>
2021-01-22 01:14:07 -0300

    aa

--- includes/_pagina_videos.php
+++ includes/_pagina_videos.php
@@ -1,7 +1,9 @@
 <div class="titulo"><div class="nombre">Videos</div></div>
+<div class="cont_video_resp">
 <?php for($i=0; $i<count($inicio['videos']); $i++){ ?>
-<div class="video-responsive">
+<div class="video_resp">
     <iframe src="https://www.youtube.com/embed/<?php echo $inicio['videos'][$i]['urls']; ?>" frameborder="0" allowfullscreen></iframe>
 </div>
 <?php } ?>
+</div>
 
