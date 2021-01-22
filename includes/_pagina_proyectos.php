886dbd2198a0300ed33fde384c2d94f2758a5bf4
jhygt46 <infolifeoperacional@gmail.com>
2021-01-22 02:48:20 -0300

    aa

--- includes/_pagina_proyectos.php
+++ includes/_pagina_proyectos.php
@@ -1,12 +1,12 @@
 <div class="titulo"><div class="nombre">Proyectos</div></div>
 <div class="cont_static">
-    <?php for($i=0; $i<count($inicio['proyectos']); $i++){ ?>
+    <?php foreach ($inicio['proyectos'] as $valor){ ?>
         <div class="static">
             <div class="row clearfix">
-                <div class="row1"><?php echo $inicio['proyectos'][$i]['foto'][0]; ?></div>
+                <div class="row1"><?php echo $valor['foto'][0]; ?></div>
                 <div class="row2">
-                    <div class="row2a"><?php echo $inicio['proyectos'][$i]['nombre']; ?></div>
-                    <div class="row2b"><?php echo $inicio['proyectos'][$i]['descripcion']; ?></div>
+                    <div class="row2a"><?php echo $valor['nombre']; ?></div>
+                    <div class="row2b"><?php echo $valor['descripcion']; ?></div>
                 </div>
             </div>
         </div>
