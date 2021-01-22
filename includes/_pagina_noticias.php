4ec6d45b6b2399f8baea85f0ce52b4f42b522615
jhygt46 <infolifeoperacional@gmail.com>
2021-01-22 02:45:55 -0300

    aa

--- includes/_pagina_noticias.php
+++ includes/_pagina_noticias.php
@@ -1,13 +1,14 @@
 <div class="titulo"><div class="nombre">Noticias</div></div>
-<?php
-    echo "<pre>";
-    print_r($inicio['noticias']);
-    echo "</pre>";
-?>
 <div class="cont_static">
     <?php for($i=0; $i<count($inicio['noticias']); $i++){ ?>
         <div class="static">
-            <div class="row"></div>
+            <div class="row clearfix">
+                <div class="row1"><?php echo $inicio['noticias'][$i]['foto'][0]; ?></div>
+                <div class="row2">
+                    <div class="row2a"><?php echo $inicio['noticias'][$i]['nombre']; ?></div>
+                    <div class="row2b"><?php echo $inicio['noticias'][$i]['descripcion']; ?></div>
+                </div>
+            </div>
         </div>
     <?php } ?>
 </div>
--- includes/_pagina_proyectos.php
+++ includes/_pagina_proyectos.php
@@ -1,13 +1,14 @@
 <div class="titulo"><div class="nombre">Proyectos</div></div>
-<?php
-    echo "<pre>";
-    print_r($inicio['proyectos']);
-    echo "</pre>";
-?>
 <div class="cont_static">
     <?php for($i=0; $i<count($inicio['proyectos']); $i++){ ?>
         <div class="static">
-            <div class="row"></div>
+            <div class="row clearfix">
+                <div class="row1"><?php echo $inicio['proyectos'][$i]['foto'][0]; ?></div>
+                <div class="row2">
+                    <div class="row2a"><?php echo $inicio['proyectos'][$i]['nombre']; ?></div>
+                    <div class="row2b"><?php echo $inicio['proyectos'][$i]['descripcion']; ?></div>
+                </div>
+            </div>
         </div>
     <?php } ?>
 </div>
