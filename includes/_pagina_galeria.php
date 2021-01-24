4c14d2738aa224d86f1bd062059db5f9f905502e
jhygt46 <infolifeoperacional@gmail.com>
2021-01-24 18:51:43 -0300

    aa

--- includes/_pagina_galeria.php
+++ includes/_pagina_galeria.php
@@ -1,13 +1,8 @@
 <div class="titulo"><div class="nombre">Galerias</div></div>
-<?php
-    echo "<pre>";
-    print_r($inicio['galeria']);
-    echo "</pre>";
-?>
 <div class="cont_static">
     <div class="static">
         <?php for($i=0; $i<count($inicio['galeria']); $i++){ ?>
-
+            <div><img src="https://www.usinox.cl/foto.php?archivo=<?php echo $_SERVER["REQUEST_SCHEME"]; ?>://<?php echo $_SERVER["HTTP_HOST"]; ?>/uploads/images/<?php echo $inicio['galeria'][$i]['nombre']; ?>&ancho=218&alto=180" alt="<?php echo $inicio['galeria'][$i]['alt']; ?>" /></div>
         <?php } ?>
     </div>
 </div>
