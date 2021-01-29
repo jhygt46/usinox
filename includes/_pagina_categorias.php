a61fc6185cc02e2cddaf67b1ab6ca7cee93a3aa5
jhygt46 <infolifeoperacional@gmail.com>
2021-01-29 17:52:11 -0300

    aa

--- includes/_pagina_categorias.php
+++ includes/_pagina_categorias.php
@@ -1,3 +1,8 @@
+<?php
+    echo "<pre>";
+    print_r($inicio);
+    echo "</pre>";
+?>
 <div class="titulo"><div class="nombre"><?php if(isset($inicio['parents'])){ echo $inicio['parents']; } ?> <?php echo $inicio['nombre']; ?></div></div>
 <div class="lista_productos clearfix">
 <?php if(isset($inicio['childs_cat'])){ for($i=0; $i<count($inicio['childs_cat']); $i++){ $foto = ($inicio['childs_cat'][$i]['foto'] == "") ? "sin_imagen.jpg" : $inicio['childs_cat'][$i]['foto'] ; ?>
