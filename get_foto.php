b60a4b517bb58481321ff0bc4b892e851666c22d
jhygt46 <infolifeoperacional@gmail.com>
2021-02-03 19:17:50 -0300

    aa

---
+++ get_foto.php
@@ -0,0 +1,10 @@
+<?php
+
+    $file = explode(".", $_GET["file"]);
+    $url = "http://www.usinox.cl/admin/imagenes/".$_GET["file"];
+    $img1 = "/uploads/images/".$file[0]."0.".$file[1];
+    $img2 = "/uploads/images/".$file[0]."1.".$file[1];
+    file_put_contents($img1, file_get_contents($url));
+    file_put_contents($img2, file_get_contents($url));
+
+?>
