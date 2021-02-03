3d769abc1c462e85a73151fadd73446523dbd5a6
jhygt46 <infolifeoperacional@gmail.com>
2021-02-03 19:48:26 -0300

    aa

--- get_foto.php
+++ get_foto.php
@@ -7,6 +7,8 @@
     file_put_contents($img1, file_get_contents($url));
     file_put_contents($img2, file_get_contents($url));
 
+    echo $url;
+
     file_put_contents("get_foto.txt", $img1."\n", FILE_APPEND);
     file_put_contents("get_foto.txt", $img2."\n", FILE_APPEND);
 
