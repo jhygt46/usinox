97783039484cd2d0038aa671948c96002332b0cf
jhygt46 <infolifeoperacional@gmail.com>
2021-02-03 22:28:12 -0300

    aa

--- includes/_pagina_categorias.php
+++ includes/_pagina_categorias.php
@@ -1,7 +1,9 @@
 <?php
+    /*
     echo "<pre>";
     print_r($inicio);
     echo "</pre>";
+    */
 ?>
 <div class="titulo"><div class="nombre"><?php if(isset($inicio['parents'])){ echo $inicio['parents']; } ?> <?php echo $inicio['nombre']; ?></div></div>
 <div class="lista_productos clearfix">
