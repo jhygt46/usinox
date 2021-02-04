95a437f75ede6beaee3059e24a797a8e3dd4c8d7
jhygt46 <infolifeoperacional@gmail.com>
2021-02-03 21:33:39 -0300

    aa

--- get_foto.php
+++ get_foto.php
@@ -1,20 +1,17 @@
 <?php
 
     $fotos = json_decode(file_get_contents("http://www.usinox.cl/lista_fotos.json"));
-
-    echo "<pre>";
-    print_r($fotos);
-    echo "</pre>";
-
-    /*
-    for($i=0; $i<count($fotos); $i++){
-        $file = explode(".", $fotos[$i]);
-        $url = "http://www.usinox.cl/admin/imagenes/".$fotos[$i];
-        $img1 = "uploads/images/".$file[0]."0.".$file[1];
-        $img2 = "uploads/images/".$file[0]."1.".$file[1];
-        file_put_contents($img1, file_get_contents($url));
-        copy($img1, $img2);
+    
+    for($i=1; $i<count($fotos); $i++){
+        if($fotos[$i] != ""){
+            $file = explode(".", $fotos[$i]);
+            $url = "http://www.usinox.cl/admin/imagenes/".$fotos[$i];
+            $img1 = "uploads/images/".$file[0]."0.".$file[1];
+            $img2 = "uploads/images/".$file[0]."1.".$file[1];
+            file_put_contents($img1, file_get_contents($url));
+            copy($img1, $img2);
+        }
     }
-    */
+    
 
 ?>
